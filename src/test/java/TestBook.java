5a94b8e21e074999bd75371a2ea85608d8ec6688
GuillaumeLandwerlin58 <guillaume.landwerlin@gmail.com>
2023-03-28 15:00:45 +0200

    Mise en place des tests

--- src/test/java/TestBook.java
+++ src/test/java/TestBook.java
@@ -1,3 +1,16 @@
-public class TestBook {
+import org.testng.annotations.Test;
+import org.junit.jupiter.params.provider.CsvSource;
 
+public class TestBook {
+    @CsvSource({
+            "The Hobbit, J.R.R. Tolkien, 1937",
+            "The Lord of the Rings, J.R.R. Tolkien, 1954",
+            "The Silmarillion, J.R.R. Tolkien, 1977"
+    })
+    public void testBook(String title, String author, String date) {
+        Book book = new Book(title, author, date);
+        System.out.println(book.getTitle());
+        System.out.println(book.getAuthor());
+        System.out.println(book.getDate());
+    }
 }
